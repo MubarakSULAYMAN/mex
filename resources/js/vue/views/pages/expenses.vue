9c37c9775c37f1fbafe885aa31e459343ed8ee37
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-12-05 15:05:00 +0100

    Revert "Revert "responsiveness improved, restarting backend""
    This reverts commit 792e7a253297732241627543e652f2ba2bb0f4c1.
    
    returning to 'safemode'

--- resources/js/vue/views/pages/expenses.vue
+++ resources/js/vue/views/pages/expenses.vue
@@ -3,30 +3,30 @@
         <the-shortcuts />
 
         <button
-            class="fixed bottom-10 right-10 py-2 px-6 rounded-md text-white font-light text-2xl bg-purple-700 transform transition duration-500 ease-in-out hover:bg-transparent hover:shadow-lg hover:text-purple-700 focus:outline-none focus:ring-8 focus:ring-purple-500 focus:ring-opacity-50 border hover:border-purple-700 hover:bg-purple-100 z-50"
+            class="fixed bottom-6 md:bottom-10 right-6 md:right-10 py-1 md:py-2 px-3 md:px-6 rounded-md text-white font-light md:text-2xl bg-purple-700 transform transition duration-500 ease-in-out hover:bg-transparent hover:shadow-lg hover:text-purple-700 focus:outline-none focus:ring-2 md:focus:ring-8 focus:ring-purple-500 focus:ring-opacity-50 border hover:border-purple-700 hover:bg-purple-100 z-50"
             @click="$router.push('expenses/modify')">
             <font-awesome-icon class="hover:text-yellow-700 mr-2" :icon="['fas', 'file-invoice']" />
             Modify
         </button>
 
-        <div class="p-12">
-            <p class="group text-purple-700 font-light text-xl inline-block"> Here are the existing records. <span class="text-xs font-medium opacity-0 group-hover:opacity-100">Click the button below
+        <div class="p-4 md:p-12">
+            <p class="group text-purple-700 font-light md:text-xl inline-block"> Here are the existing records. <span class="text-xs font-normal md:font-medium opacity-0 group-hover:opacity-100">Click the button below
                     to modify.</span> </p>
-            <table class="table-fixed shadow-lg bg-white border-collapse border hover:border-purple-700">
+            <table class="table-fixed shadow-lg bg-white border-collapse border hover:border-purple-700 text-xs md:text-base md:font-normal">
                 <tr class="bg-purple-700 border border-collapse text-white text-center">
-                    <th class="s-n px-8 py-4"> S/N </th>
-                    <th class="w-3/5 px-8 py-4"> Narration </th>
-                    <th class="category-col px-8 py-4"> Category </th>
-                    <th class="w-1/5 px-8 py-4"> Amount </th>
-                    <th class="w-1/5 px-8 py-4"> Date of Expenditure </th>
+                    <th class="s-n p-1 md:px-8 md:py-4"> S/N </th>
+                    <th class="w-3/5 p-1 md:px-8 md:py-4"> Narration </th>
+                    <th class="category-col p-1 md:px-8 md:py-4"> Category </th>
+                    <th class="w-1/5 p-1 md:px-8 md:py-4"> Amount </th>
+                    <th class="w-1/5 p-1 md:px-8 md:py-4"> Date of Expenditure </th>
                 </tr>
-                <tr class="border border-collapse even:border-purple-700 font-medium text-purple-700 text-center transform transition duration-500 ease-in-out hover:bg-purple-500 hover:text-white">
-                    <td class="border px-8 py-4"> 1
+                <tr class="border border-collapse even:border-purple-700 font-normal md:font-medium text-purple-700 text-center transform transition duration-500 ease-in-out hover:bg-purple-500 hover:text-white">
+                    <td class="border p-1 md:px-8 md:py-4"> 1
                     </td>
-                    <td class="border px-8 py-4"> Some superstory about fantasy. </td>
-                    <td class="border px-8 py-4"> Food </td>
-                    <td class="border px-8 py-4"> NGN5,000 </td>
-                    <td class="border px-8 py-4"> 12/12/2020 </td>
+                    <td class="border p-1 md:px-8 md:py-4"> Some superstory about fantasy. </td>
+                    <td class="border p-1 md:px-8 md:py-4"> Food </td>
+                    <td class="border p-1 md:px-8 md:py-4"> NGN5,000 </td>
+                    <td class="border p-1 md:px-8 md:py-4"> 12/12/2020 </td>
                 </tr>
             </table>
         </div>
