975462de67a88ba7fa8d792ad22f672df05bb598
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-12-10 18:29:28 +0100

    modified package.json and 'assets' changed to 'secure_asset'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
         <title>Expense Manager</title>
 
         <!-- Fonts -->
         {{-- <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> --}}
         {{-- <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"> --}}
-        <link rel="icon" href="{{ URL::asset('report.svg') }}" type="image/x-icon"/>
+        <link rel="icon" href="{{ URL::secure_asset('report.svg') }}" type="image/x-icon"/>
 
         {{-- <link rel="shortcut icon" type="image/png" href="../../resources/js/vue/assets/img/personal_finance.svg"/> --}}
 
