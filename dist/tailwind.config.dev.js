41c011e1741c6dcd90b2b49dc30a0e17aa177b9e
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2021-01-21 08:00:04 +0100

    minor fix

---
+++ dist/tailwind.config.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = {
+  purge: ['./resources/**/*.vue'],
+  darkMode: false,
+  // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'ubuntu': ["'Ubuntu'", 'sans-serif']
+      }
+    }
+  },
+  variants: {
+    extend: {
+      // tableLayout: ['hover', 'focus'],
+      // divideColor: ['group-hover'],
+      // display: ['hover', 'focus'],
+      borderRadius: ['first', 'last']
+    }
+  },
+  plugins: []
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     purge: [
-        //     './resources/**/*.vue',
+        './resources/**/*.vue',
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
